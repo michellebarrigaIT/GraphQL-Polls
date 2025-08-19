31d73cad2b4f96f86c40dd9e820c71f8df65273e
michellebarrigaIT <michelle.barriga@assuresoft.tech>
2025-08-19 09:37:09 -0400

    feat: add resolver test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppResolver } from './resolver.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.gql',
+      playground: true,
+    }),
+  ],
+  providers: [AppResolver],
 })
 export class AppModule {}
